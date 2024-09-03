f9c281ca1412eb8fcb5b95ab0f511279f63b2a7f
Abby Sypniewski <59975659+abbysyp@users.noreply.github.com>
2024-09-03 14:40:56 -0400

    Update script.js with download function

--- DigiArch/public/javascripts/script.js
+++ DigiArch/public/javascripts/script.js
@@ -1,3 +1,19 @@
+// this is a function that downloads a string to a text file in browser
+// to your local Downloads folder
+function downloadString(text, filename, mimeType) {
+    var blob = new Blob([text], { type: mimeType });
+    var url = window.URL.createObjectURL(blob);
+  
+    var a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+  
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }
+
 // this is a function that will collect values from metadata form,
 // put them into JSON format, then write and download to metadata.txt file
 function convertToJson() {
@@ -10,13 +26,14 @@ function convertToJson() {
             formData[element.name] = element.value;
         }
     }
-    
+
     // convert to string format and add newlines for aesthetic
     let jsonData = JSON.stringify(formData, "", " ");
 
     // print to console log in browser
     console.log(jsonData)
-    
-    // create and write to a file called metadata.txt, then download in Chrome
-    download(jsonData, "metadata.txt", "text/plain")
+
+    // create and write to a file called metadata.txt, then download in browser
+    downloadString(jsonData, "metadata.txt", "text/plain")
+
 }
